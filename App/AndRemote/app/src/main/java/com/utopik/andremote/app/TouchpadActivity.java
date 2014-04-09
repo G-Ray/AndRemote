a6779696916ebf56e6a5face79a79dc7e2bd53af
G-Ray <geoffrey.bonneville@gmail.com>
2014-04-09 21:50:30 +0200

    Add TouchpadActivity with a TouchpadEventView

--- App/AndRemote/app/src/main/java/com/utopik/andremote/app/TouchpadActivity.java
+++ App/AndRemote/app/src/main/java/com/utopik/andremote/app/TouchpadActivity.java
@@ -12,6 +12,7 @@ public class TouchpadActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_touchpad);
+        //setContentView(new TouchpadEventView(this, null));
     }
 
 
---
+++ App/AndRemote/app/src/main/java/com/utopik/andremote/app/TouchpadEventView.java
@@ -0,0 +1,52 @@
+package com.utopik.andremote.app;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by geoffrey on 4/9/14.
+ */
+public class TouchpadEventView extends View {
+    private Paint paint = new Paint();
+    private Path path = new Path();
+
+    public TouchpadEventView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(6f);
+        paint.setColor(Color.BLUE);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPath(path, paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float eventX = event.getX();
+        float eventY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(eventX, eventY);
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(eventX, eventY);
+                break;
+            default:
+                return false;
+        }
+        invalidate();
+        return true;
+    }
+}
