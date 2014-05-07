a4b9bc24d1cf63a9348c74396d1b77b721a0d345
G-Ray <geoffrey.bonneville@gmail.com>
2014-05-07 12:02:40 +0200

    Remove unused imports

--- App/AndRemote/app/src/main/java/com/utopik/andremote/app/Command.java
+++ App/AndRemote/app/src/main/java/com/utopik/andremote/app/Command.java
@@ -2,11 +2,7 @@ package com.utopik.andremote.app;
 
 import android.util.Log;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.net.UnknownHostException;
 
 /**
  * Created by geoffrey on 07/05/14.
